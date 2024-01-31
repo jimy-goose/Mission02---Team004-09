c6561f237b47356c85e63dfd49af62d902b5dc5d
jimy-goose <131339722+jimy-goose@users.noreply.github.com>
2024-01-31 14:42:00 -0700

    here it is

--- Program.cs
+++ Program.cs
@@ -3,5 +3,31 @@
     public static void Main()
     {
         Console.WriteLine("Welecome to Tic Tac Toe");
+
+        string[] board = new string[9];
+
+        string playerChoice = "";
+
+        string winnerName = "";
+
+        bool done = false;
+
+        do
+        {
+            Console.WriteLine("Player 1's Turn");
+            string p1Choice = Console.ReadLine();
+            
+
+            
+            Console.WriteLine(printboard());
+            done, winnerName = CheckWinner();
+            
+            Console.WriteLine("Player 2's Turn");
+            string p2Choice = Console.ReadLine();
+            Console.WriteLine(printboard());
+            done, winnerName = CheckWinner();
+
+        } while (!done);
+        
     }
 }
