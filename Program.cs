39b59bf9e63cd32997a9d3167e47ac9801e57e2c
jimy-goose <131339722+jimy-goose@users.noreply.github.com>
2024-01-31 17:14:39 -0700

    ddd

--- Program.cs
+++ Program.cs
@@ -16,16 +16,22 @@
         {
             Console.WriteLine("Player 1's Turn");
             string p1Choice = Console.ReadLine();
+
+            if (p1Choice == "a1")
+            {
+                board[0] = "x";
+
+            }
             
 
             
             Console.WriteLine(printboard());
-            done, winnerName = CheckWinner();
+            done, winnerName = CheckBoard();
             
             Console.WriteLine("Player 2's Turn");
             string p2Choice = Console.ReadLine();
             Console.WriteLine(printboard());
-            done, winnerName = CheckWinner();
+            done, winnerName = CheckBoard();
 
         } while (!done);
         
