cac9c5e57685325699bb8b709678200c7f31c6e7
jimy-goose <131339722+jimy-goose@users.noreply.github.com>
2024-02-02 16:34:06 -0700

    PUUSSSHHHH

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ public class Driver
     public static void Main()
     {
 
-       
+
         Console.WriteLine("Welecome to Tic Tac Toe");
 
         string[] board = new string[9];
@@ -27,14 +27,14 @@ public class Driver
         int guesser = 0;
         do
         {
-            Console.WriteLine("Player" + (I +1) + "s Turn (e.g. a1, b3");
+            Console.WriteLine("Player " + (I + 1) + "s Turn (e.g. a1, b3");
             playerChoice = Console.ReadLine();
             char letterChoice = playerChoice[0];
             char numberChoice = playerChoice[1];
-            
-            int intNumberChoice = int.Parse(numberChoice.ToString());
 
-            if (letterChoice == 97 || letterChoice == 98 || letterChoice == 99 && numberChoice > 48 && numberChoice < 52)
+            
+
+            if ((letterChoice == 97 || letterChoice == 98 || letterChoice == 99) && (numberChoice > 48 && numberChoice < 52))
             {
                 if (Array.IndexOf(guessed, playerChoice) != -1)
                 {
@@ -43,6 +43,7 @@ public class Driver
                 }
                 else
                 {
+                    int intNumberChoice = int.Parse(numberChoice.ToString());
                     if (letterChoice == 97)
                     {
                         cell = intNumberChoice - 1;
@@ -62,13 +63,10 @@ public class Driver
                         Console.WriteLine(b.ChangeBoard(board));
                         Console.ReadLine();
                         I += 1;
-                        Console.WriteLine(b.checkBoard(board));
-                        if(b.checkBoard(board) == true)
+                        if (b.checkBoard(board) == true)
                         {
-                            Console.WriteLine("Yay! Player " + (I) + " Wins");
                             done = true;
                         }
-
                     }
                     else if (I == 1)
                     {
@@ -78,25 +76,23 @@ public class Driver
                         Console.WriteLine(b.ChangeBoard(board));
                         Console.ReadLine();
                         I -= 1;
-                        Console.WriteLine(b.checkBoard(board));
                         if (b.checkBoard(board) == true)
                         {
-                            Console.WriteLine("Yay! Player " + (I + 2) + " Wins");
                             done = true;
                         }
                     }
                     guesser++;
-                    
+
                 }
             }
             else
             {
                 Console.WriteLine("Invalid guess try again");
             }
-               
+
 
         } while (!done);
-        
+
     }
-   
+
 }
